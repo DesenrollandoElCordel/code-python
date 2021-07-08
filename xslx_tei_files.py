2f6577976417bb4e1e2a1667b1caf751f7582017
Elina Leblanc <elina.leblanc@live.fr>
2021-07-08 14:06:58 +0200

    Modification file

--- xslx_tei_files.py
+++ xslx_tei_files.py
@@ -3,69 +3,85 @@ from lxml import etree
 import os
 
 
-def traitement_motsclefs(motclef, element):
-    if motclef is not None:
-        if "," in motclef:
-            motclefs_personajes_split = motclef.split(", ")
-            for x in motclefs_personajes_split:
-                item = etree.SubElement(element, "item", corresp="")
-                item.text = x
+# Fonction pour la gestion des mots-clés
+def add_keywords(keyword, category, element):
+    if keyword is not None:
+        if "," in keyword:  # Gestion des catégories avec plusieurs mots-clés
+            keyword_split = keyword.split(",")
+            for k in keyword_split:
+                catref = etree.SubElement(element, "catRef", scheme="#" + category, target="#" + k)
         else:
-            item = etree.SubElement(element, "item", corresp="")
-            item.text = motclef
+            catref = etree.SubElement(element, "catRef", scheme="#" + category, target="#" + keyword)
 
 
-wb = load_workbook(filename='index_grabados.xlsx')
+wb = load_workbook(filename='')
 index = wb['Feuil1']
+# print(index)
 
+# Récupération des noms de colonnes
+list_name_columns = []
+for row in index.iter_rows(max_row=1, max_col=9, values_only=True):
+    list_name_columns.append(row)
+# print(list_name_columns)
+
+# Récupération des descriptions de gravure
 title_engravings = []
-
-for row in index.iter_rows(min_row=2, max_row=6, max_col=6, values_only=True):
+for row in index.iter_rows(min_row=2, max_row=7, max_col=9, values_only=True):
     row_list = list(row)
     title_engravings.append(row_list)
+# print(title_engravings)
 
-print(title_engravings)
-
+# Création des fichiers .xml
 for i in range(len(title_engravings)):
-    root = etree.Element("TEI")
+    root = etree.Element("TEI", xmlns="http://www.tei-c.org/ns/1.0", xmlid=title_engravings[i][0])
+    teiHeader = etree.SubElement(root, "teiHeader")
 
-    teiHeader = etree.SubElement(root, "teiHeader", xmlns="http://www.tei-c.org/ns/1.0", xmlid=title_engravings[i][0])
     fileDesc = etree.SubElement(teiHeader, "fileDesc")
     titleStmt = etree.SubElement(fileDesc, "titleStmt")
     title = etree.SubElement(titleStmt, "title")
     title.text = title_engravings[i][0]
+
     respStmt = etree.SubElement(titleStmt, "respStmt")
     name = etree.SubElement(respStmt, "name")
     resp = etree.SubElement(respStmt, "resp")
     resp.text = "Codificación TEI"
+
     pubStmt = etree.SubElement(fileDesc, "publicationStmt")
     pubStmt_child1 = etree.SubElement(pubStmt, "authority")
     pubStmt_child1.text = "Bibliothèque Universitaire de Genève (BUNIGE)"
     pubStmt_child2 = etree.SubElement(pubStmt, "availability")
     pubStmt_child2_p = etree.SubElement(pubStmt_child2, "p")
     pubStmt_child2_p.text = "CC-BY-NC-SA"
+
     sourceDesc = etree.SubElement(fileDesc, "sourceDesc")
-    sourceDesc_p = etree.SubElement(sourceDesc, "p")
-    sourceDesc_p.text = "Grabado extraído del pliego " + title_engravings[i][1]
-
-    text = etree.SubElement(root, "text")
-    body = etree.SubElement(text, "body")
-    div = etree.SubElement(body, "div")
-    figure = etree.SubElement(div, "figure")
-
-    graphic = etree.SubElement(figure, "graphic", url=title_engravings[i][0]+".jpg")
-
+    bibl = etree.SubElement(sourceDesc, "bibl")
+    ident = etree.SubElement(bibl, "ident", type="DOI")
+    figure = etree.SubElement(bibl, "figure")
+    graphic = etree.SubElement(figure, "graphic", url="")
     figDesc = etree.SubElement(figure, "figDesc")
     locus1 = etree.SubElement(figDesc, "locus", target=title_engravings[i][1]+".xml")
     locus1.text = title_engravings[i][2]
     date = etree.SubElement(figDesc, "date")
     date.text = str(title_engravings[i][3])
+    note1 = etree.SubElement(bibl, "note", type="similar_ejemplar")
+    note2 = etree.SubElement(bibl, "note", type="origen")
+    note2.text = "Grabado extraído del pliego " + title_engravings[i][1]
 
-    keywords_personajes = etree.SubElement(figDesc, "list", type="personajes", xmlbase="http://vocab.getty.edu/page/aat/")
-    traitement_motsclefs(title_engravings[i][4], keywords_personajes)
+    encodingDesc = etree.SubElement(teiHeader, "encodingDesc")
+    projectDesc = etree.SubElement(encodingDesc, "projectDesc")
+    paragraph = etree.SubElement(projectDesc, "p")
+    paragraph.text = "Este archivo fue creado en el marco del proyecto Desenrollando el Cordel/Démêler le cordel/Untangling the cordel, dirigido por la profesora Constance Carta de la Universidad de Ginebra, con el apoyo de la Fundación filantrópica Famille Sandoz-Monique de Meuron."
 
-    keywords_objetos = etree.SubElement(figDesc, "list", type="objetos", xmlbase="http://vocab.getty.edu/page/aat/")
-    traitement_motsclefs(title_engravings[i][5], keywords_objetos)
+    profileDesc = etree.SubElement(teiHeader, "profileDesc")
+    textClass = etree.SubElement(profileDesc, 'textClass')
+    add_keywords(title_engravings[i][5], list_name_columns[0][5], textClass)  # Catégorie 'personaje_masculino'
+    add_keywords(title_engravings[i][6], list_name_columns[0][6], textClass)  # Catégorie 'personaje_femenino'
+    add_keywords(title_engravings[i][7], list_name_columns[0][7], textClass)  # Catégorie 'accesorios'
+    add_keywords(title_engravings[i][8], list_name_columns[0][8], textClass)  # Catégorie 'lugar'
+
+    text = etree.SubElement(root, "text")
+    body = etree.SubElement(text, "body")
+    paragraph2 = etree.SubElement(body, "p")
 
     tree = etree.ElementTree(root)
 
@@ -77,7 +93,7 @@ for i in range(len(title_engravings)):
     # print(filename)
 
     filename_path = os.path.join(path_tei, filename)
-    # print(filename_path)
+    print(filename_path)
 
     if not os.path.isfile(filename_path):
         tree.write(filename_path, xml_declaration=True, encoding='UTF-8', pretty_print=True)
