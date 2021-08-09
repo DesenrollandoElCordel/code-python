0ad1fa11705428d9a3afe579f1f00d96db46adab
Elina Leblanc <elina.leblanc@live.fr>
2021-08-09 10:18:54 +0200

    modification code

--- insertion_xml_img.py
+++ insertion_xml_img.py
@@ -24,13 +24,8 @@ for f in os.listdir(args.xml):
         for pb in root.findall('.//tei:pb', ns):  # On itère à travers chaque <pb/>
             # print(pb)
             numberPage = pb.get('n')  # On récupère @n
-            url = 'https://iiif.unige.ch/demelercordel/'  # On crée le 'base URI'
-            pb.set('facs', url + label + '_' + numberPage + '.jpg')  # On ajoute un @facs à chaque <pb/> avec pour valeur le nouvel URI
+            # url = 'https://iiif.unige.ch/demelercordel/'  # On crée le 'base URI'
+            pb.set('source', label + '_' + numberPage + '.jpg')  # On ajoute un @source à chaque <pb/> avec pour valeur le nouvel URI
 
-        new_folder = "encodage_updated/"  # On crée un nouveau dossier pour enregistrer les nouveaux .xml
-        if not os.path.isdir(new_folder):  # On vérifie s'il existe. Si non, on le crée
-            os.mkdir(new_folder)
-
-        new_path = os.path.join(new_folder, f)  # Création du nouveau chemin vers les nvx fichiers .xml
         # print(new_path)
-        tree.write(new_path, encoding="UTF-8", xml_declaration=True)  # Création des nvx arbres .xml et enregistrement dans le nouveau dossier !
+        tree.write(xml_path, encoding="UTF-8", xml_declaration=True)  # Création des nvx arbres .xml et enregistrement dans le nouveau dossier !
