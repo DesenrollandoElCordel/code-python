43a6b2199e83521af05509fe9f0bb108ebb08066
Elina Leblanc <elina.leblanc@live.fr>
2022-03-31 17:19:45 +0200

    add lines for engravings

--- creation_thumbnail.py
+++ creation_thumbnail.py
@@ -3,7 +3,7 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--xml', default='../Encodage/Moreno-TEI-files', type=str, help="Le chemin du dossier des fichiers xml à charger.")
+parser.add_argument('--xml', default='../Encodage/engravings-catalogue/xml-files', type=str, help="Le chemin du dossier des fichiers xml à charger.")
 args = parser.parse_args()
 
 for filename in os.listdir(args.xml):
@@ -17,12 +17,22 @@ for filename in os.listdir(args.xml):
         tree = eT.parse(xml_path)
         root = tree.getroot()
 
+        # For engravings catalog
+        img_graphic = root.find(".//tei:figure/tei:graphic", ns)
+        iiif_uri_graphic = img_graphic.get("url")
+        iiif_uri_thumbnail = iiif_uri_graphic[:-18] + "200," + iiif_uri_graphic[-14:]
+
+        thumbnail_graphic = root.find(".//tei:facsimile/tei:graphic", ns)
+        if thumbnail_graphic is not None:
+            thumbnail_graphic.set("url", iiif_uri_thumbnail)
+        print(label + ": done")
+
+        # for pliegos files
         for pb in root.findall(".//tei:pb", ns):
             pageNumber = pb.get("n")
             if pageNumber == "1":
                 iiif_uri = pb.get("facs")
                 iiif_thumbnail_uri = iiif_uri[:22] + "200," + iiif_uri[26:]
-                # print(iiif_thumbnail_uri)
                 graphic = root.find(".//tei:facsimile/tei:graphic", ns)
                 if graphic is not None:
                     graphic.set("url", iiif_thumbnail_uri)
