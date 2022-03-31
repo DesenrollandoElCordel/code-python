93c99382dfb90bf9e7908ecd6c63bec0fa35954b
Elina Leblanc <elina.leblanc@live.fr>
2022-03-31 15:19:13 +0200

    Update script (IIIF Uri)

--- creation_thumbnail.py
+++ creation_thumbnail.py
@@ -1,29 +1,27 @@
-import cv2
+import xml.etree.ElementTree as eT
 import os
 
-path = 'images'
+path = '../Encodage/TEI_tests/'
 
 for filename in os.listdir(path):
-    if not filename.endswith('Store'):
-        img_path = os.path.join(path, filename)
+    if filename.endswith('.xml'):
+        xml_path = os.path.join(path, filename)
         label, ext = os.path.splitext(filename)
 
-        if label.endswith('1'):
-            image = cv2.imread(img_path)
+        ns = {'tei': 'http://www.tei-c.org/ns/1.0'}
+        eT.register_namespace('', 'http://www.tei-c.org/ns/1.0')
 
-            r = 150 / image.shape[0]
-            dim = (int(image.shape[1] * r), 150)
+        tree = eT.parse(xml_path)
+        root = tree.getroot()
 
-            thumbnail = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+        for pb in root.findall(".//tei:pb", ns):
+            pageNumber = pb.get("n")
+            if pageNumber == "1":
+                iiif_uri = pb.get("facs")
+                iiif_thumbnail_uri = iiif_uri[:22] + "150," + iiif_uri[26:]
+                # print(iiif_thumbnail_uri)
+                graphic = root.find(".//tei:facsimile/tei:graphic", ns)
+                graphic.set("url", iiif_thumbnail_uri)
+                print(label + ": done")
 
-            path_thumbnail = 'thumbnails/'
-            if not os.path.isdir(path_thumbnail):
-                os.mkdir(path_thumbnail)
-
-            new_name = label[:-2] + '_thumbnail' + ext
-            # print(new_name)
-            img_thumbnail_path = os.path.join(path_thumbnail, new_name)
-            # print(img_thumbnail_path)
-
-            if not os.path.isfile(img_thumbnail_path):
-                cv2.imwrite(img_thumbnail_path, thumbnail)
+        tree.write(xml_path, encoding="UTF-8", xml_declaration=True)
