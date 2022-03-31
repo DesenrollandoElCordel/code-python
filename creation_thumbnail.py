68dbb14cf67245326d4d443d2c123a81f9e637a6
Elina Leblanc <elina.leblanc@live.fr>
2022-03-31 15:28:11 +0200

    Update script (argparse)

--- creation_thumbnail.py
+++ creation_thumbnail.py
@@ -1,11 +1,14 @@
 import xml.etree.ElementTree as eT
 import os
+import argparse
 
-path = '../Encodage/TEI_tests/'
+parser = argparse.ArgumentParser()
+parser.add_argument('--xml', default='../Encodage/Moreno-TEI-files', type=str, help="Le chemin du dossier des fichiers xml à charger.")
+args = parser.parse_args()
 
-for filename in os.listdir(path):
+for filename in os.listdir(args.xml):
     if filename.endswith('.xml'):
-        xml_path = os.path.join(path, filename)
+        xml_path = os.path.join(args.xml, filename)
         label, ext = os.path.splitext(filename)
 
         ns = {'tei': 'http://www.tei-c.org/ns/1.0'}
@@ -18,10 +21,11 @@ for filename in os.listdir(path):
             pageNumber = pb.get("n")
             if pageNumber == "1":
                 iiif_uri = pb.get("facs")
-                iiif_thumbnail_uri = iiif_uri[:22] + "150," + iiif_uri[26:]
+                iiif_thumbnail_uri = iiif_uri[:22] + "200," + iiif_uri[26:]
                 # print(iiif_thumbnail_uri)
                 graphic = root.find(".//tei:facsimile/tei:graphic", ns)
-                graphic.set("url", iiif_thumbnail_uri)
+                if graphic is not None:
+                    graphic.set("url", iiif_thumbnail_uri)
                 print(label + ": done")
 
         tree.write(xml_path, encoding="UTF-8", xml_declaration=True)
