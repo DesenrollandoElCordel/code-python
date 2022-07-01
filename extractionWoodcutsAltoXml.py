b91697ebd253cab974e2b7949ab2377ab2b973c7
Elina Leblanc <elina.leblanc@live.fr>
2022-07-01 12:10:09 +0200

    Find informations about pliegos

--- extractionWoodcutsAltoXml.py
+++ extractionWoodcutsAltoXml.py
@@ -1,7 +1,15 @@
 import os
 import xml.etree.ElementTree as et
+from openpyxl import Workbook
+
+wb = Workbook()
+wb_filename = "../Index_Grabados_Varios.xlsx"
+
+ws1 = wb.active
+ws1.title = "metadata"
 
 list_xml = "../OCR_Training/Varios-OCR-files"
+list_woodcuts = []
 
 for subdir, dirs, files in os.walk(list_xml):
     for file in files:
@@ -9,13 +17,13 @@ for subdir, dirs, files in os.walk(list_xml):
         # print(xml_path)
         label, ext = os.path.splitext(file)
         # print(label)
-        list_coord_str = []
         ns = {'alto': 'http://www.loc.gov/standards/alto/ns-v4#'}
 
         if file.endswith('.xml'):
             document = et.parse(xml_path)
             root = document.getroot()
             # print(root)
+            woodcutCounter = 0
 
             for ImgBlock in root.findall('.//alto:TextBlock[@TAGREFS="BT224"]', ns):
                 xStart = int(ImgBlock.get('HPOS')[:-2])
@@ -23,6 +31,18 @@ for subdir, dirs, files in os.walk(list_xml):
                 xEnd = int(ImgBlock.get('WIDTH')[:-2]) + xStart
                 yEnd = int(ImgBlock.get('HEIGHT')[:-2]) + yStart
                 coords = str(xStart) + "," + str(yStart) + "," + str(xEnd) + "," + str(yEnd)
-                # print(coords)
 
-                # TO DO : Find titles, id pliego, image number // Put the data in Excel file
+                '''pliegoTitle = root.find('.//alto:TextBlock[@TAGREFS="BT223"]/alto:TextLine/alto:String/[@CONTENT]', ns)
+                if pliegoTitle:
+                    print(label, pliegoTitle)'''
+
+                for ImgName in root.findall('.//alto:fileName', ns):
+                    pliegoName = ImgName.text[:-6]
+                    pageName = ImgName.text[:-4]
+                    woodcutCounter = woodcutCounter + 1
+                    woodcutName = 'grabado_v' + ImgName.text[6:-5] + str(woodcutCounter)
+                    for i in range(len(ImgBlock)):
+                        list_woodcuts.append([woodcutName, pliegoName, pageName, coords])
+
+
+# wb.save(filename=wb_filename)
