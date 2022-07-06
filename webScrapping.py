a78c8e129187318938dd13c7ea9b36909ed16fdd
Elina Leblanc <elina.leblanc@live.fr>
2022-07-06 11:37:47 +0200

    Initial commit

---
+++ webScrapping.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+url = 'https://cudl.lib.cam.ac.uk/collections/spanishchapbooks/1'
+
+
+
+'''print(r)
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+div = soup.find('h5')
+# div2 = div.find('div')
+# div3 = div2.find('div', class_='collections_carousel_text')
+# title_h5 = div3.find('h5')
+
+print(div)'''
