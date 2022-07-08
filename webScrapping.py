a94aacb1968e0702f62e657fa6bad42d65f0de14
Elina Leblanc <elina.leblanc@live.fr>
2022-07-08 11:07:26 +0200

    Scrap titles with Selenium

--- webScrapping.py
+++ webScrapping.py
@@ -1,20 +1,34 @@
 from bs4 import BeautifulSoup
-import requests
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-url = 'https://cudl.lib.cam.ac.uk/collections/spanishchapbooks/1'
+element_list = []
+url_list = ['https://cudl.lib.cam.ac.uk/collections/spanishchapbooks/1']
 
+for url in url_list:
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(url)
+    time.sleep(1)
+    html = driver.page_source
+    # title = driver.find_elements(By.NAME, 'h5')
+    soup = BeautifulSoup(html, "html.parser")
+    title = soup.find_all('h5')
+    for t in title:
+        element_list.append(t.text)
+    document_page = soup.find_all('div', {'class': 'collections_carousel_text'})
+    for i in document_page:
+        document_page_link = driver.find_element(By.LINK_TEXT, "a")
+        # document_page_link = i.find('a')
+        # driver.findElement(By.linkText("App Configuration")).click()
+        document_page_link.click()
+        # print(document_page_link)
 
+# print(element_list)
+driver.close()
 
-'''print(r)
+# TO DO : Récupérer les URL des notices des documents et essayer de scrapper les informations avec BeautifulSoup.
+# Important => Manifeste est dans la code source. On peut donc le scrapper sans Selenium !!
 
-soup = BeautifulSoup(r.content, 'html.parser')
-
-div = soup.find('h5')
-# div2 = div.find('div')
-# div3 = div2.find('div', class_='collections_carousel_text')
-# title_h5 = div3.find('h5')
-
-print(div)'''
