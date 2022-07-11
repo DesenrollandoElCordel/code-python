f474d436e8d90b66fed9ba41c39312710670dace
Elina Leblanc <elina.leblanc@live.fr>
2022-07-11 16:42:58 +0200

    Get IIIF manifests + Excel Storage

--- webScrapping.py
+++ webScrapping.py
@@ -3,32 +3,61 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+import requests
+from openpyxl import Workbook
 
-element_list = []
-url_list = ['https://cudl.lib.cam.ac.uk/collections/spanishchapbooks/1']
+wb = Workbook()
+wb_filename = "../cudl_data.xlsx"
 
-for url in url_list:
+ws = wb.active
+
+url_base = 'https://cudl.lib.cam.ac.uk/collections/spanishchapbooks/'
+titles_list = []
+item_url_list = []
+manifests_list = []
+
+for url in range(1, 3):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get(url)
-    time.sleep(1)
+    driver.get(url_base + str(url))
+    time.sleep(5)
     html = driver.page_source
-    # title = driver.find_elements(By.NAME, 'h5')
     soup = BeautifulSoup(html, "html.parser")
-    title = soup.find_all('h5')
-    for t in title:
-        element_list.append(t.text)
-    document_page = soup.find_all('div', {'class': 'collections_carousel_text'})
-    for i in document_page:
-        document_page_link = driver.find_element(By.LINK_TEXT, "a")
-        # document_page_link = i.find('a')
-        # driver.findElement(By.linkText("App Configuration")).click()
-        document_page_link.click()
-        # print(document_page_link)
+    titles = soup.find_all('h5')
+    for title in titles:
+        titles_list.append(title.text)
+
+    row = 1
+    for t in titles_list:
+        ws.cell(column=1, row=row, value=t)
+        row += 1
+
+    item_page = soup.find_all('div', {'class': 'collections_carousel_text'})
+    for i in item_page:
+        item_page_link = i.find('a')
+        item_url_list.append('https://cudl.lib.cam.ac.uk' + item_page_link.get('href'))
+        # print(document_page_link.get('href'))
+
+for item_url in item_url_list:
+    req = requests.get(item_url)
+    soup2 = BeautifulSoup(req.text, 'html.parser')
+    iiif_button = soup2.find('div', {'id': 'iiifOption'})
+    # print(iiif_button)
+    iiif_div = iiif_button.find('div', {'class': 'button'})
+    iiif_url = iiif_button.find('a', {'class': 'btn'}).get('href')
+    manifest = iiif_url.split("?")
+    manifests_list.append(manifest[1])
+    # print(manifest[1])
+
+row2 = 1
+for m in manifests_list:
+    ws.cell(column=2, row=row2, value=m)
+    row2 += 1
+
 
 # print(element_list)
+# print(item_url_list)
 driver.close()
 
-# TO DO : Récupérer les URL des notices des documents et essayer de scrapper les informations avec BeautifulSoup.
-# Important => Manifeste est dans la code source. On peut donc le scrapper sans Selenium !!
+wb.save(filename=wb_filename)
 
+# TO DO : Lire les informations concernant lieu, date et imprimeur depuis le fichier JSON
