deca9a8101ce47deb95ec0e2d93085c327a32a76
Elina Leblanc <elina.leblanc@live.fr>
2022-07-14 13:04:00 +0200

    Get JSON data

--- webScrapping.py
+++ webScrapping.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
 from openpyxl import Workbook
+import json
 
 wb = Workbook()
 wb_filename = "../cudl_data.xlsx"
@@ -12,30 +13,25 @@ wb_filename = "../cudl_data.xlsx"
 ws = wb.active
 
 url_base = 'https://cudl.lib.cam.ac.uk/collections/spanishchapbooks/'
-titles_list = []
-item_url_list = []
-manifests_list = []
 
-for url in range(1, 3):
+item_url_list = []
+item_data = []
+manifests_url = []
+test_list = []
+
+for url in range(27, 28):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(url_base + str(url))
     time.sleep(5)
     html = driver.page_source
     soup = BeautifulSoup(html, "html.parser")
-    titles = soup.find_all('h5')
-    for title in titles:
-        titles_list.append(title.text)
-
-    row = 1
-    for t in titles_list:
-        ws.cell(column=1, row=row, value=t)
-        row += 1
 
     item_page = soup.find_all('div', {'class': 'collections_carousel_text'})
     for i in item_page:
         item_page_link = i.find('a')
         item_url_list.append('https://cudl.lib.cam.ac.uk' + item_page_link.get('href'))
         # print(document_page_link.get('href'))
+# print(item_url_list)
 
 for item_url in item_url_list:
     req = requests.get(item_url)
@@ -45,19 +41,38 @@ for item_url in item_url_list:
     iiif_div = iiif_button.find('div', {'class': 'button'})
     iiif_url = iiif_button.find('a', {'class': 'btn'}).get('href')
     manifest = iiif_url.split("?")
-    manifests_list.append(manifest[1])
-    # print(manifest[1])
+    manifests_url.append(manifest[1][9:])
+    item_data.append([manifest[1][9:]])
 
-row2 = 1
-for m in manifests_list:
-    ws.cell(column=2, row=row2, value=m)
-    row2 += 1
+for m in manifests_url:
+    jsonFile = json.loads(requests.get(m).text)
+    '''date = jsonFile["metadata"][0]["value"]
+    printer = jsonFile["metadata"][8]["value"]
+    title = jsonFile["metadata"][6]["value"]'''
 
+    place = ""
+    title = ""
 
-# print(element_list)
-# print(item_url_list)
+    for i in jsonFile["metadata"]:
+        # print(i.values())
+
+        if "Place of Publication" in i.values():
+            places = list(i.values())
+            place = places[1].split(";")
+            # test_list.append(place[1])
+            # print(place[1])
+        if "Date of Publication" in i.values():
+            date = list(i.values())[1]
+            # print(date)
+        if "Title" in i.values():
+            title = list(i.values())[1]
+        if "Publisher" in i.values():
+            printer = list(i.values())[1]
+    test_list.append([place[1], title])
+
+print(test_list)
 driver.close()
 
-wb.save(filename=wb_filename)
+# wb.save(filename=wb_filename)
 
 # TO DO : Lire les informations concernant lieu, date et imprimeur depuis le fichier JSON
