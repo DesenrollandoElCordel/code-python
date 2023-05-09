2919b1d5f5f85042eb7ae7d2ccd4881992ea28d4
Elina Leblanc <elina.leblanc@live.fr>
2023-05-09 17:59:39 +0200

    Update script with CSV elements

--- ner2tei.py
+++ ner2tei.py
@@ -1,77 +1,89 @@
+import os
 import xml.etree.ElementTree as eT
+import csv
+import re
 
-ner_file = "../Encodage/moreno_035/Moreno_035.conll"
-xml_file = "../Encodage/Moreno-TEI-files/Moreno_035.xml"
+xml_folder = "../Encodage/moreno_035"
+new_csvLine = []
 
-with open(ner_file, 'r') as f:
-    text = f.read()
+for file in os.listdir(xml_folder):
+    if file.endswith('.xml'):
+        xml_path = os.path.join(xml_folder, file)
 
-ner_list_raw = []
-ner_list_normalized = []
-ner_list_deduplicated = []
+        ns = {'tei': 'http://www.tei-c.org/ns/1.0'}
+        eT.register_namespace('', 'http://www.tei-c.org/ns/1.0')
 
-# On découpe le texte par ligne
-new_text = text.split('\n')
+        tree = eT.parse(xml_path)  # On parse le fichier XML-TEI
+        root = tree.getroot()  # On récupére la racine du fichier TEI
+        id_doc = root.get("{http://www.w3.org/XML/1998/namespace}id")  # On récupère l'id du document
 
-# Pour chaque ligne, si elle finit par 'LOC', on l'ajoute à la liste ner_list_raw
-for line in new_text:
-    if line.endswith('LOC'):
-        ner_list_raw.append(line)
+        with open('../Encodage/moreno_035/nerList_v2.csv', encoding='utf-8') as f:
+            csv_file = csv.reader(f)
+            next(csv_file)  # On passe la première ligne
 
-# Boucle spéciale pour les noms de lieux avec plusieurs tokens (B-LOC + I-LOC)
-# On récupére l'index (i) et chaque élément (e) de ner_list_raw
-for i, e in enumerate(ner_list_raw):
-    # print(i, '->', e)
-    # On cible uniquement les éléments qui finissent par 'I-LOC' et dont l'élément précédent est un 'B-LOC'
-    if e.endswith('I-LOC') and ner_list_raw[i-1].endswith('B-LOC'):
-        new_ILOC = e.replace(" I-LOC", "")  # On enlève 'I-LOC'
-        new_BLOC = ner_list_raw[i-1].replace(" B-LOC", "")  # On enlève 'B-LOC'
-        ner_list_normalized.append(new_BLOC + " " + new_ILOC)  # On concatène les tokens pour reformer le nom de lieu
-        ner_list_raw.remove(e)  # On enlève l'élément avec 'I-LOC' de ner_list_raw
-        ner_list_raw.pop(i-1)  # On enlève l'élément avec 'B-LOC' de ner_list_raw
-        # print(ner_list_raw[i-1] + " " + e)
-# print(ner_list_raw)
+            loc_list_deduplicated = []  # Initialisation d'une liste avec les noms originaux sans doublon
+            loc_list_normalized_all = []  # Initialisation d'une liste avec tous les noms normalisés
+            # loc_list_normalized = []  # Initialisation d'une liste avec les noms normalisés sans doublon
 
-# Boucle pour les autres noms de lieux
-for l in ner_list_raw:
-    new_loc = l.replace(" B-LOC", "")  # On enlève 'B-LOC'
-    ner_list_normalized.append(new_loc)  # On ajoute les noms de lieux à ner_list_normalized
-# print(ner_list_normalized)
-ner_list_normalized.sort()  # On trie les lieux par ordre alphabétique
+            for line in csv_file:
+                if line[1].lower() == id_doc:
+                    loc_list_normalized_all.append([line[3], line[5]])
+                    # if line[3] not in loc_list_normalized:
+                        # loc_list_normalized.append(line[3])
+                    if line[2] not in loc_list_deduplicated:
+                        loc_list_deduplicated.append(line[2])
 
-# On crée une liste spéciale sans doublons
-for item in ner_list_normalized:
-    if item not in ner_list_deduplicated:
-        ner_list_deduplicated.append(item)
-# print(ner_list_deduplicated)
+                    shortTitle = root.find('.//tei:titleStmt/tei:title', ns).text
+                    shortTitle = re.sub(r"\n( +)", " ", shortTitle)
+                    printer = root.find('.//tei:publisher', ns).text
+                    pubPlace = root.find('.//tei:pubPlace', ns).text
+                    typeText = root.find('.//tei:keywords/tei:term', ns).text
 
-tree = eT.parse(xml_file)  # On parse le fichier XML-TEI
-root = tree.getroot()  # On récupére la racine du fichier TEI
+                    line.append(shortTitle)
+                    line.append(pubPlace)
+                    line.append(printer)
+                    line.append(typeText)
+                    new_csvLine.append(line)
+                    # print(shortTitle)
 
-# Déclaration du namespace
-ns = {'tei': 'http://www.tei-c.org/ns/1.0'}
-eT.register_namespace('', 'http://www.tei-c.org/ns/1.0')
+            total = {}
+            for k, v in loc_list_normalized_all:
+                total[k] = str(total.get(k,v))
+            loc_list_normalized = [list(t) for t in total.items()]
 
-# Encodage des noms de lieu dans le texte
-for l in root.findall(".//tei:l", ns):
-    # On récupère l'index de chaque élément de la liste
-    for i in range(len(ner_list_deduplicated)):
-        # print(ner_list[i])
-        # On vérifie que le nom de lieu est dans le texte et on l'encode avec <name>
-        if ner_list_deduplicated[i] in l.text:
-            new_line = l.text.replace(ner_list_deduplicated[i], '<name>' + ner_list_deduplicated[i] + '</name>')
-            l.text = new_line
+            for l in root.findall(".//tei:l", ns):
+                # print(l.text)
+                for i in range(len(loc_list_deduplicated)):
+                    # print(ner_list[i])
+                    # On vérifie que le nom de lieu est dans le texte et on l'encode avec <name>
+                    if loc_list_deduplicated[i] in l.text:
+                        new_line = l.text.replace(loc_list_deduplicated[i], '<name>' + loc_list_deduplicated[i] + '</name>')
+                        l.text = new_line
+        # eT.dump(root)
 
-# Encodage des noms de lieu dans le teiHeader
-sourceDesc = root.find('.//tei:sourceDesc', ns)  # On récupère l'élément <sourceDesc>
-listPlace = eT.SubElement(sourceDesc, 'listPlace')  # On ajoute à <sourceDesc> l'élément <listPlace>
+            sourceDesc = root.find('.//tei:sourceDesc', ns)  # On récupère l'élément <sourceDesc>
+            listPlace = eT.SubElement(sourceDesc, 'listPlace')  # On ajoute à <sourceDesc> l'élément <listPlace>
+            # print(loc_list_normalized_all)
+            for place in loc_list_normalized:
+                placeElement = eT.SubElement(listPlace, "place")  # On ajoute l'élément <place>
+                placeNb = loc_list_normalized_all.count(place)  # On compte le nb de fois où le nom de lieu apparaît dans le texte
+                placeElement.set("n", str(placeNb))  # On ajoute à <placeName> un @n avec le nb d'occurrences du lieu
 
-for place in ner_list_deduplicated:
-    placeElement = eT.SubElement(listPlace, "place")  # On ajoute l'élément <place>
-    placeName = eT.SubElement(placeElement, "placeName")  # Puis l'élément <placeName>
-    placeName.text = place.title()  # On ajoute à <placeName> les noms de lieux en rétablissant les majuscules
-    placeNb = ner_list_normalized.count(place)  # On compte le nb de fois où le nom de lieu apparaît dans le texte
-    placeElement.set("n", str(placeNb))  # On ajoute à <placeName> un @n avec le nb d'occurrences du lieu
+                placeName = eT.SubElement(placeElement, "placeName")  # On ajoute l'élément <placeName>
+                placeName.text = place[0].title()  # On ajoute à <placeName> les noms de lieux en rétablissant les majuscules
 
-# eT.dump(sourceDesc)
-# tree.write(xml_file, encoding="UTF-8", xml_declaration=True)
+                location = eT.SubElement(placeElement, "location")
+                geo = eT.SubElement(location, "geo")
+                geo.text = place[1][6:-1]
+
+        eT.dump(listPlace)
+        print('\n')
+        # tree.write(xml_path, encoding="UTF-8", xml_declaration=True)
+
+new_csv_header = ['index', 'id_doc', 'original_name', 'normalized_name', 'id_wkd', 'coord', 'type_place',
+                  'shortTitle', 'pubPlace', 'printer', "type_text"]
+
+# with open('../Encodage/moreno_035/nerList_v3.csv', 'w', encoding='utf-8', newline='') as newCsv:
+    # writer = csv.writer(newCsv)
+    # writer.writerow(new_csv_header)
+    # writer.writerows(new_csvLine)
